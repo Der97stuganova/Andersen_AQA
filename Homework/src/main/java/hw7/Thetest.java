1427fed312f2ce46862f35bfac752d636fc977bb
Der97stuganova <86957187+Der97stuganova@users.noreply.github.com>
2021-08-22 22:18:21 +0300

    Delete Homework/src/main/java/hw7 directory

--- Homework/src/main/java/hw7/Thetest.java
+++
@@ -1,9 +0,0 @@
-package hw7;
-
-public class Thetest {
-    public static void main(String[] args) {
-        AppData appData = new AppData();
-        RWCSFile.saveDataToFile(appData);
-        RWCSFile.readDataFromFile(RWCSFile.CSVFile);
-    }
-}
